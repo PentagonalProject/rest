f70551b9b501a4521ed5b43f2470356c3ed32f87
Pentagonal <pentagonaldevelopment@gmail.com>
2017-08-03 22:47:01 +0700

    Add Simpler Way for routes (Example)

--- Components/Routes/UserRoutes.php
+++ Components/Routes/UserRoutes.php
@@ -3,14 +3,24 @@ namespace {
 
     use Apatis\ArrayStorage\CollectionFetch;
     use Pentagonal\PhPass\PasswordHash;
+    use PentagonalProject\App\Rest\Abstracts\ResponseGeneratorAbstract;
     use PentagonalProject\App\Rest\Generator\Response\Json;
+    use PentagonalProject\App\Rest\Generator\ResponseStandard;
+    use PentagonalProject\App\Rest\Util\Domain\Verify;
     use PentagonalProject\Model\Database\User;
     use Psr\Http\Message\ResponseInterface;
     use Psr\Http\Message\ServerRequestInterface;
 
     $this->post('/users', function (ServerRequestInterface $request, ResponseInterface $response) {
+        /**
+         * @var CollectionFetch $collectionFetch that use ArrayAccess
+         * that mean acess data just need to pass by array bracket
+         *
+         * -> $collectionFetch[keyName]
+         */
         $collectionFetch = new CollectionFetch((array) $request->getParsedBody());
         try {
+            // data to check
             $check = [
                 'first_name',
                 'last_name',
@@ -18,34 +28,55 @@ namespace {
                 'email',
                 'password'
             ];
+
+            $domain = new Verify();
             foreach ($check as $toCheck) {
+                // do check email
                 if ($toCheck === 'email') {
+                    // doing validate email with real valid email address
+                    $email = $domain->validateEmail(
+                        (string) $collectionFetch['email']
+                    );
+                    if (!$email) {
+                        throw new InvalidArgumentException(
+                            "Invalid email address",
+                            E_USER_ERROR
+                        );
+                    }
+                    // set fix email
+                    $collectionFetch->set('email', $email);
+                    continue;
                 }
+
+                // validate data that must be string
+                // even though exists or not it will be check
+                if (!is_string($collectionFetch[$toCheck])) {
+                    throw new InvalidArgumentException(
+                        sprintf(
+                            "Invalid %s",
+                            ucwords(str_replace('_', ' ', $toCheck))
+                        )
+                    );
+                }
+
+                //  doing next validation
+                // ..................
             }
-            /**
-             * Create a new user
-             *
-             * @var string $firstName
-             * @var string $lastName
-             * @var string $email
-             * @var string password
-             */
-            $firstName = $request->getParsedBody()['first_name'];
-            $lastName = $request->getParsedBody()['last_name'];
-            $username = $request->getParsedBody()['username'];
-            $email = $request->getParsedBody()['email'];
-            $password = $request->getParsedBody()['password'];
 
             // Instantiate password hash
             $passwordHash = new PasswordHash();
             $user = new User([
-                'first_name' => $firstName,
-                'last_name' => $lastName,
-                'username' => $username,
-                'email' => $email,
-                'password' => $passwordHash->hash(sha1($password)),
+                'first_name' => $collectionFetch['first_name'],
+                'last_name' => $collectionFetch['last_name'],
+                'username' => $collectionFetch['username'],
+                'email' => $collectionFetch['email'],
+                'password' => $passwordHash->hash(sha1($collectionFetch['password'])),
+                /**
+                 * @uses microtime() has enough to generate very unique data double float
+                 */
                 'private_key' => hash('sha512', microtime())
             ]);
+
             $user->saveOrFail();
 
             /**
@@ -54,21 +85,29 @@ namespace {
              * @var string $userId
              * @var ResponseInterface $response
              */
-            $userId = $user->getKey();
-            $response = $response->withStatus(201);
             $response = $response->withHeader(
                 'Location',
                 (string)$request->getUri()->withPath('users/' . $userId)
             );
-
-            return Json::generate($request, $response)
-                ->setData([
-                    'code' => $response->getStatusCode(),
-                    'status' => 'success',
-                    'data' => $userId
-                ])
-                ->serve(true);
+            return ResponseStandard::withData(
+                $request,
+                // response & data can pass here to prevent more memory usage on variable
+                $response->withStatus(201),
+                $user->getKey(),
+                Json::class,
+                /**
+                 * magic additional arguments for @uses ResponseGeneratorAbstract::serve()
+                 */
+                true
+            );
         } catch (Exception $e) {
+            return ResponseStandard::withData(
+                $request,
+                $response->withStatus(500),
+                $e,
+                Json::class, // or just put null as value
+                true // add pretty print on Json Generator
+            );
         }
     });
 }

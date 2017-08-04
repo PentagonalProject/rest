b85ac38caf79a88a83cb51ae511f087705d8f28f
Zahroul Ulum <moh.zahroul@gmail.com>
2017-08-04 23:08:15 +0700

    Simplify update recipe validations It still need to be refactored due the similarity with save recipe validations

--- Components/Routes/RecipeRoutes.php
+++ Components/Routes/RecipeRoutes.php
@@ -79,9 +79,7 @@ namespace {
                     }
                 }
 
-                /**
-                 * Create a new recipe
-                 */
+                // Instantiate recipe
                 $recipe = new Recipe([
                     'name'         => $requestBody['name'],
                     'instructions' => $requestBody['instructions'],
@@ -130,60 +128,54 @@ namespace {
     $this->post(
         '/recipes/{id}',
         function (ServerRequestInterface $request, ResponseInterface $response, array $params) {
+            /**
+             * Make request body fetchable.
+             *
+             * @var CollectionFetch $requestBody
+             */
+            $requestBody = new CollectionFetch((array) $request->getParsedBody());
+
             try {
-                // Put collection to FetchAble
-                $bodyParsed = new CollectionFetch($request->getParsedBody());
+                $check = [
+                    'name',
+                    'instructions',
+                    'user_id'
+                ];
 
-                // Validate recipe name
-                $name = $bodyParsed->get('name');
+                foreach ($check as $toCheck) {
+                    // Check whether data is string
+                    if (!is_string($requestBody[$toCheck])) {
+                        throw new InvalidArgumentException(
+                            sprintf(
+                                "Recipe %s should be as a string, %s given.",
+                                ucwords(str_replace('_', ' ', $toCheck)),
+                                gettype($requestBody[$toCheck])
+                            ),
+                            E_USER_WARNING
+                        );
+                    }
 
-                // Check whether recipe name is string
-                if (!is_string($name)) {
-                    throw new InvalidArgumentException(
-                        sprintf(
-                            "Recipe Name should be as a string, %s given.",
-                            gettype($name)
-                        ),
-                        E_USER_WARNING
-                    );
-                }
+                    // Check whether data is not empty
+                    if (trim($requestBody[$toCheck]) == '') {
+                        throw new InvalidArgumentException(
+                            sprintf(
+                                "Recipe %s should not be empty.",
+                                ucwords(str_replace('_', ' ', $toCheck))
+                            ),
+                            E_USER_WARNING
+                        );
+                    }
 
-                // Check whether recipe name is not empty
-                if (trim($name) == '') {
-                    throw new InvalidArgumentException(
-                        "Recipe Name should not be empty.",
-                        E_USER_WARNING
-                    );
-                }
-
-                // Check whether recipe name is not more than 60 characters
-                if (strlen($name) > 60) {
-                    throw new LengthException(
-                        "Recipe Name should not more than 60 characters.",
-                        E_USER_WARNING
-                    );
-                }
-
-                // Validate recipe instructions
-                $instructions = $bodyParsed->get('instructions');
-
-                // Check whether recipe instructions is not empty
-                if (trim($instructions) == '') {
-                    throw new InvalidArgumentException(
-                        "Recipe Instructions should not be empty",
-                        E_USER_WARNING
-                    );
-                }
-
-                // Validate recipe user id
-                $userId = $bodyParsed->get('user_id');
-
-                // Check whether recipe user id is not empty
-                if (trim($userId) == '') {
-                    throw new InvalidArgumentException(
-                        "Recipe User Id should not be empty",
-                        E_USER_WARNING
-                    );
+                    // Check name
+                    if ($toCheck === 'name') {
+                        // Check whether recipe name is not more than 60 characters
+                        if (strlen($requestBody['name']) > 60) {
+                            throw new LengthException(
+                                "Recipe Name should not more than 60 characters.",
+                                E_USER_WARNING
+                            );
+                        }
+                    }
                 }
 
                 // Get a recipe by id
@@ -191,9 +183,9 @@ namespace {
 
                 // Update found recipe
                 $recipe->update([
-                    'name'         => $name,
-                    'instructions' => $instructions,
-                    'user_id'      => $userId
+                    'name'         => $requestBody['name'],
+                    'instructions' => $requestBody['instructions'],
+                    'user_id'      => $requestBody['user_id']
                 ]);
 
                 return ResponseStandard::withData(

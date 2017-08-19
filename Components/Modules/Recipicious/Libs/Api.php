508822c34f456a4a1f9e8dbf4de07bf47e00beda
Pentagonal <pentagonaldevelopment@gmail.com>
2017-08-19 18:24:13 +0700

    Move partial API to RouteMapperBaseAbstract

--- Components/Modules/Recipicious/Libs/Api.php
+++ Components/Modules/Recipicious/Libs/Api.php
@@ -29,55 +29,20 @@ declare(strict_types=1);
 
 namespace PentagonalProject\Modules\Recipicious\Lib;
 
+use PentagonalProject\App\Rest\Abstracts\RouteMapperBaseAbstract;
 use PentagonalProject\Modules\Recipicious\Recipicious;
 use Slim\App;
-use Slim\Interfaces\RouteGroupInterface;
 
 /**
  * Class Api
  * @package PentagonalProject\Modules\Recipicious\Lib
- *
- * @method RouteGroupInterface get(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface post(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface patch(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface head(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface put(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface delete(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface options(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface trace(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface view(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface purge(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface copy(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface lock(string $pattern, callable $callable, \Closure $routeCallback = null)
- * @method RouteGroupInterface unlock(string $pattern, callable $callable, \Closure $routeCallback = null)
  */
-class Api
+class Api extends RouteMapperBaseAbstract
 {
     /**
      * @var Recipicious
      */
     protected $module;
-
-    /**
-     * @var array
-     */
-    protected $lastRoutes = [];
-
-    const AVAILABLE_METHODS = [
-        'GET',
-        'POST',
-        'PUT',
-        'HEAD',
-        'PATCH',
-        'DELETE',
-        'OPTIONS',
-        "VIEW",
-        "PURGE",
-        "COPY",
-        'PURGE',
-        'LOCK',
-        'UNLOCK',
-    ];
 
     /**
      * Api constructor.
@@ -98,73 +63,19 @@ class Api
     }
 
     /**
-     * @param array $methods
-     * @param string $pattern
-     * @param callable $callback
-     * @param \closure|null $routeCallback as route for as params
-     *
-     * @return RouteGroupInterface
+     * {@inheritdoc}
      */
-    public function map(
-        array $methods,
-        string $pattern,
-        $callback,
-        \closure $routeCallback = null
-    ) : RouteGroupInterface {
-        /**
-         * @var App $app
-         */
-        $app = $this->module->getContainer()->get('app');
-        $this->lastRoutes = func_get_args();
-        $c =& $this;
-        return $app->group(
-            $this->module->getGroupPattern(),
-            function () use ($c) {
-                /**
-                 * @var App $app
-                 */
-                $app = $this;
-                $console = $c->lastRoutes;
-                $result = $app->map(
-                    $console[0],
-                    $console[1],
-                    $console[2]
-                );
-                if (isset($console[3]) && $console[3] instanceof \Closure) {
-                    $console[3]($result, $c);
-                }
-            }
-        );
-    }
-
-    /**
-     * @param string $pattern
-     * @param callable $callback
-     * @param \closure|null $routeCallback
-     *
-     * @return mixed
-     */
-    public function any(
-        string $pattern,
-        $callback,
-        \closure $routeCallback = null
-    ) {
-        $args = func_get_args();
-        array_unshift($args, self::AVAILABLE_METHODS);
-        return call_user_func_array([$this, 'map'], $args);
-    }
-
-    /**
-     * @param string $name
-     * @param array $arguments
-     *
-     * @return mixed
-     */
-    public function __call(string $name, array $arguments)
+    public function getApp(): App
     {
-        // add arguments
-        array_unshift($arguments, [strtoupper($name)]);
-        $result = call_user_func_array([$this, 'map'], $arguments);
-        return $result;
+        $app = $this->module->getContainer()['app'];
+        return $app;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGroupPattern(): string
+    {
+        return $this->module->getGroupPattern();
     }
 }
